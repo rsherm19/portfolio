82515417cdb22521833cbffcf2fd9130fd5da3c5
Ryan Sherman <rsherman5@elon.edu>
2024-11-01 22:22:11 -0400

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useState } from "react"
+imp
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-black h-[100px]'>
+      
+    </div>
   )
 }
 
